10a976442e130534bb2fab302eb7fb41b75997e2
Victor Contreras <victorknight34@gmail.com>
2026-02-19 17:07:13 -0600

    first commit

---
+++ src/p2_examen1_victorcontreras/P2_Examen1_VictorContreras.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p2_examen1_victorcontreras;
+
+/**
+ *
+ * @author victo
+ */
+public class P2_Examen1_VictorContreras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
